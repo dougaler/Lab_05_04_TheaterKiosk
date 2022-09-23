ffac713e635a25ac3cb01b9bf55c241080ea667d
dougaler <dougaler@mail.uc.edu>
2022-09-23 12:04:59 -0400

    Completed Theater Kiosk Task

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,9 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        System.out.println("How old are you?");
+        int age = 23;
+        if(age >= 21) {
+            System.out.println("You get a wristband.");
+        }
+    }
+}
